3f3ee64a9a793af287043752d76f3fb3515742d1
Ambroise <ambroise.marigot.stag@axa.com>
2016-05-12 16:15:58 +0200

    deleted stuff

--- basics/test.js
+++
@@ -1,7 +0,0 @@
-function createAdder (baseNumber) {
-	return function (numberToAdd) {
-		return numberToAdd + baseNumber;
-	};
-}
-
-console.log(createAdder(100)(2));
--- password-manager/test.js
+++
@@ -1,9 +0,0 @@
-setTimeout(function () {
-	console.log('3');
-}, 2000);
-
-setTimeout(function () {
-	console.log('2');
-}, 1000);
-
-console.log('1');
