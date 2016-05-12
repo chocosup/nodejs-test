f4ec5f3634b61c50254c774e0ac54c500fea9059
Ambroise <ambroise.marigot.stag@axa.com>
2016-05-12 16:29:22 +0200

    test

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/', function (req, res) {
 	res.send('API root.');
 });
 
+app.get('/test', function (req, res) {
+	res.send('testing.');
+});
+
 app.listen(PORT, function () {
 	console.log('app listening on port ' + PORT + '.');
 });
